e887020416d6bd3f2afca1e7938b56e580895e90
Jesper Nord <jesper@nord.pm>
2023-11-29 13:16:13 +0100

    introduce tailwind; add some styling to start page

--- src/routes/Start/index.jsx
+++ src/routes/Start/index.jsx
@@ -1,5 +1,6 @@
 import { fetchTasks } from '../../http';
 import { Link, useLoaderData } from 'react-router-dom';
+import { format } from 'date-fns';
 
 export async function loader() {
   const tasks = await fetchTasks();
@@ -18,19 +19,31 @@ export const Start = () => {
   }
 
   return (
-    <div>
+    <div className="p-6 bg-slate-100 w-screen h-screen">
+      <div className="text-xl font-medium text-black mb-6">Recurring tasks</div>
       {tasks.map((task) => (
-        <div key={task.id}>
-          <Link to={`/task/${task.id}`}>{task.name}</Link>
-          {task.history.length > 0 && (
-            <div>
-              {' - '}
-              {task.history[0].completed_at}
+        <div
+          key={task.id}
+          className="p-4 mx-auto mb-4 bg-white rounded-md shadow-md flex flex-row items-center"
+        >
+          <div className="flex flex-1 flex-col">
+            <Link to={`/task/${task.id}`} className="text-lg text-slate-800">
+              {task.name}
+            </Link>
+            <div className="text-sm text-gray-500">
+              {task.history.length > 0
+                ? format(new Date(task.history[0].completed_at), 'Pp')
+                : 'Never completed'}
             </div>
-          )}
+          </div>
         </div>
       ))}
-      <Link to="/task">Create new</Link>
+      <Link
+        to="/task"
+        className="absolute bottom-6 right-6 bg-gray-600 rounded-md p-2 text-gray-200"
+      >
+        <button className="button">New task</button>
+      </Link>
     </div>
   );
 };
