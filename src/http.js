1e1a8383fe981fd0d003747dfaad4b7ac49192d5
Jesper Nord <jesper@nord.pm>
2023-12-22 14:29:11 +0100

    update api base path

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAccessToken, getRefreshToken, saveTokens } from './util';
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: '/recurringly/api',
 });
 
 client.interceptors.request.use(
